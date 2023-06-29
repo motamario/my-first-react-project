d258fc5ece0cfca99e5ed856c42671268657f2ca
Mario Mota <rosasmario24@gmail.com>
2023-06-29 09:46:23 -0600

    modified function app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import {puppyList} from './data.js'
-
+import './App.css';
+import './index.css';
 
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
-  console.log("puppyList: ", puppyList);
+  
+  
 
   return (
     <div className="App">
       {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+        featPupId && (
+          <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+        )
+      }
+
+      {puppies.map((puppy) => {
+
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+        function handleClick() {
+          // some logic here
+          console.log("puppy id: ", puppy.id)
+          setFeatPupId(puppy.id)
+        }
+      })}
     </div>
   );
 }
